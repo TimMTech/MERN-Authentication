f32d58d4b93b2c37c0c9664f769205c42a4d4aa7
TimMTech <tmukhamedov97@gmail.com>
2022-05-31 17:45:45 -0400

    Updating for Heroku Launch...

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,36 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const path = require("path")
-const routerUrls = require("./Routes/routes")
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const routerUrls = require("./Routes/routes");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-__dirname = path.resolve()
+__dirname = path.resolve();
 
-dotenv.config({path: path.resolve(__dirname, "../.env")})
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 mongoose.connect(process.env.DATABASE_ACCESS, () => {
-    console.log("DB CONNECTED")
-})
+  console.log("DB CONNECTED");
+});
 
-
- 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use("/auth", routerUrls);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/fontend/build")))
-    app.get("*", (req,res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-    })
+  app.use(express.static(path.join(__dirname, "/fontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 } else {
-    app.get("/", (req, res) => {
-      res.send("API RUNNING");
-    });
+  app.get("/", (req, res) => {
+    res.send("API RUNNING");
+  });
+  app.post("/", (req, res) => {
+    res.send("API IS RUNNING");
+  });
 }
 
-
-
-
-
-app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT ${PORT}`))
+app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT ${PORT}`));
