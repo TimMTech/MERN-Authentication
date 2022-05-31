261baadca97bc8c3ff2df8f70f0543fcd5ddb5ee
TimMTech <tmukhamedov97@gmail.com>
2022-05-31 17:56:13 -0400

    Spelling error when trying to deploy...

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use("/auth", routerUrls);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/fontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
